469c2f582a3f9a55ed77f183a2c669774071d679
SK-54 <93460168+SK-54@users.noreply.github.com>
2021-12-22 10:22:35 +0330

    Add files via upload

---
+++ ExternalFiles/SisSeLf/config.php
@@ -0,0 +1,62 @@
+<?php
+$admin0 = 971621004; # Original Helper Admin
+$adminsSK = [965194536, 971621004, 1315949751, ]; # For Non-original Self Admins
+$token = '0999';
+$helper_username = '@000000';
+
+$use_DB = true; # if you want use MySQL DataBase, set $use_DB = true. OTHERWISE set $use_DB = false.
+if( $use_DB == true ) {
+	if( empty($DB_name) or empty($DB_user) or empty($DB_pass) )
+		die('DataBase Information Variables Are EMPTY, Edit "oth/config.php" File.');
+}
+$DB_name = ''; # DataBase Name
+$DB_user = ''; # DataBase UserName
+$DB_pass = ''; # DataBase PassWord
+
+if( $use_DB == true ) {
+	$settings = [
+	    'serialization' => [
+	        'cleanup_before_serialization' => true,
+	    ],
+	    'logger' => [
+	        'max_size' => 1*1024*1024,
+	    ],
+	    'peer' => [
+	        'full_fetch' => false,
+	        'cache_all_peers_on_startup' => false,
+	    ],
+	    'db'=> [
+	        'type'  => 'mysql',
+	        'mysql' => [
+	            'host'     => 'localhost',
+	            'port'     => '3306',
+	            'user'     => $DB_user,
+	            'password' => $DB_pass,
+	            'database' => $DB_name,
+	            'max_connections' => 5
+	        ]
+	    ],
+	    'app_info'=>[
+			'api_id'=>17044113,
+			'api_hash'=>'4b36c278ad18e1944a0b5efc964a3005'
+		]
+	];
+}else{
+	$settings = [
+	    'serialization' => [
+	        'cleanup_before_serialization' => true,
+	    ],
+	    'logger' => [
+	        'max_size' => 1*1024*1024,
+	    ],
+	    'peer' => [
+	        'full_fetch' => false,
+	        'cache_all_peers_on_startup' => false,
+	    ],
+	    'app_info'=>[
+			'api_id'=>17044113,
+			'api_hash'=>'4b36c278ad18e1944a0b5efc964a3005'
+		]
+	];
+}
+?>
